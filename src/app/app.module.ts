4b8e2b2592dd62cde70ebe367c6af2b990d1e72b
Rafael Gorges <rafaelgorges@outlook.com>
2018-09-29 15:57:55 -0300

    Limpeza de CSS e alterações para padrões do bootstrap. Iniciada criação do componente de informações de recursos do emulador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ResourcesComponent } from './resources/resources.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ResourcesComponent
   ],
   imports: [
     BrowserModule
