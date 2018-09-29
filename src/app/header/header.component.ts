b5769623fd2307a96a7f9f67e17d939c75267999
Rafael Gorges <rafaelgorges@outlook.com>
2018-09-29 14:17:48 -0300

    Adicionados efeitos de luminosidade em navbar sólida e criado menu para git do brAthena

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -13,7 +13,7 @@ export class HeaderComponent implements OnInit {
   ngOnInit() {
     $(document).ready(function() { 
       $(window).scroll(function() {
-        if($(this).scrollTop() > 800) { 
+        if($(this).scrollTop() > 180) { 
             $('.navbar').addClass('solid');
         } else {
             $('.navbar').removeClass('solid');
