5941c063c7e5b90b579aa0475707523e790e1c19
Rafael Gorges <rafaelgorges@outlook.com>
2018-09-30 02:48:14 -0300

    Criado componente de informações do novo emulador. Necessário alterar os ícones de acordo com o tipo de texto informativo.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ResourcesComponent } from './resources/resources.component';
+import { InfosComponent } from './infos/infos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ResourcesComponent
+    ResourcesComponent,
+    InfosComponent
   ],
   imports: [
     BrowserModule
